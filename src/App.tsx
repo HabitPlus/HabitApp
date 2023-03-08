ff3d8a980b752f211c6839a739ce08a940bbcee6
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-08 10:49:27 +0100

    fixed routes, it renders now

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
-import {Route, Routes} from 'react-router-dom'
-import Landing from './pages/Landing';
-import Layout from './components/Layout';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-  
+    <BrowserRouter>
       <Routes>
-        <Route element={<Layout />} />
-        <Route path='/' element= {<Landing />} />
+        <Route element={<Layout />}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        </Route>
       </Routes>
-
-      </>
+      </BrowserRouter>
+    </>
   );
 }
 
