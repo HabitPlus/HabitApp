d3b409cd5150243411248f4fc13382342bef82b0
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-10 14:34:40 +0100

    added styles to landing, plus config modified

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import chica from '../../assets/girl-home1.svg'
+import logo from '../../assets/logo-caritas.png'
 
 const Landing = () => {
   return (
-<>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button>Habit+</button>
-  </>
+<div className='flex flex-col 1 place-content: center'>
+  <img src={logo} alt="chica-corriendo" /> <br />
+  <img src={chica} alt="" />
+<br />  
+  <Link to="/home" className='font-serif text-white font-weight: 600 bg-orange rounded-xl p-3 tracking-[1em] place-content-center'>Habit+</Link>
+  </div>
   )
 }
 
