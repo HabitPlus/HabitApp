380b467d4c51c15dff7e359029592caae891d99b
albapro <albamaria.idi@gmail.com>
2023-03-13 12:21:30 +0100

    test button nutricion working

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing/Landing";
 import Layout from "./components/Layout";
 import Home from "./pages/home/Home";
+import Nutricion from "./pages/nutricion/Nutricion";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route element={<Layout />}>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/nutricion" element={<Nutricion />} />
         </Route>
       </Routes>
       </BrowserRouter>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate= useNavigate()
+  const handleClick = () => {
+    navigate("/nutricion");
+  };
+
   return (
-   
-    <button>Home</button>
+    <>
+      <button>Home</button>
+      <button onClick={handleClick} type="button"
+        className="bg-red hover:bg-green cursor-pointer px-24 py-3 flex gap-8 rounded-sm text-black absolute bottom-20">
+        Nutrición</button>
+    </>
   )
 }
 
