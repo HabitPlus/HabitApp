9ac3c1c9d800b472d360d7e5426d2bd5386c847c
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-14 13:41:03 +0100

    corrected margins for girl-logo

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -6,13 +6,12 @@ import logo from "../../assets/logo-caritas.png";
 const Landing = () => {
   return (
     <>
-      <div className="flex justify-center mt-10">
-        <img src={logo} alt="logo-caritas" /> <br />
+      <div className="flex justify-center mt-14">
+        <img src={logo} alt="logo-caritas" />
       </div>
 
-      <div className="md:flex md:items-center md:justify-center mx-10 gap-14">
-        <img src={chica} alt="chica-corriendo" className="my-20 md:w-1/3" />
-        <br />
+      <div className='flex flex-col md:flex-row items-center justify-center gap-20'>
+        <img src={chica} alt="chica-corriendo" className="mt-20 md:w-1/3" />
         <Link to="/home">
           {" "}
           <button className="font-serif text-3xl text-white font-weight: 600 bg-orange hover:bg-red rounded-full py-1 pl-4 tracking-[0.5em] ">
@@ -20,6 +19,7 @@ const Landing = () => {
           </button>
         </Link>
       </div>
+    
     </>
   );
 };
