053ee8cbe106c2508026815bb062961d2af196c7
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-14 13:02:06 +0100

    added button tag, styles correctly apllied

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -13,10 +13,12 @@ const Landing = () => {
       <div className="md:flex md:items-center md:justify-center mx-10 gap-14">
         <img src={chica} alt="chica-corriendo" className="my-20 md:w-1/3" />
         <br />
-        <Link
-          to="/home"
-          className="font-serif text-3xl text-white font-weight: 600 bg-orange rounded-full py-1 pl-4 tracking-[0.5em] "
-        >Habit+</Link>
+        <Link to="/home">
+          {" "}
+          <button className="font-serif text-3xl text-white font-weight: 600 bg-orange rounded-full py-1 pl-4 tracking-[0.5em] ">
+            Habit+
+          </button>
+        </Link>
       </div>
     </>
   );
