fa7e147389a2b4be16425095b70ec6cc45ea121b
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-14 12:50:09 +0100

    styles working

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import chica from '../../assets/girl-home1.svg'
-import logo from '../../assets/logo-caritas.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import chica from "../../assets/girl-home1.svg";
+import logo from "../../assets/logo-caritas.png";
 
 const Landing = () => {
   return (
-<div className='flex flex-col 1 place-content: center'>
-  <img src={logo} alt="chica-corriendo" /> <br />
-  <img src={chica} alt="" />
-<br />  
-  <Link to="/home" className='font-serif text-white font-weight: 600 bg-orange rounded-xl p-3 tracking-[1em] place-content-center'>Habit+</Link>
-  </div>
-  )
-}
+    <>
+      <div className="flex justify-center mt-10">
+        <img src={logo} alt="logo-caritas" /> <br />
+      </div>
 
-export default Landing
+      <div className="md:flex md:items-center md:justify-center mx-10 gap-14">
+        <img src={chica} alt="chica-corriendo" className="my-20 md:w-1/3" />
+        <br />
+        <Link
+          to="/home"
+          className="font-serif text-3xl text-white font-weight: 600 bg-orange rounded-full py-1 pl-4 tracking-[0.5em] "
+        >Habit+</Link>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
