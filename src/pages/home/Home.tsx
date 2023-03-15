ff0a71fd7e7de6a6deef0a69392415156a247cfc
victornietof5 <vihugo317f5@gmail.com>
2023-03-15 13:10:19 +0100

    modified aling botons

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,27 +11,27 @@ const Home = () => {
       <div className="gap-4 md:gap-8">
         <div className="grid grid-cols-2 grid-rows-2 md:grid-cols-4 md:grid-rows-1 gap-4 md:gap-8">
           <Link to='/nutricion'>
-            <button className="w-full h-48 md:h-72 bg-blue rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center">
-              <img className='w-1/2 md:w-3/4' src={nutrition} alt="nutrición" />
-              <p className="text-white text-xs md:text-base">Nutrición</p>
+            <button className="p-3 w-full h-52 md:h-72 bg-blue rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center gap-4">
+              <img className='w-30 md:w-40 md:mb-6' src={nutrition} alt="nutrición" />
+              <p className="text-white text-xl md:text-3xl mb-5">Nutrición</p>
             </button>
           </Link>
           <Link to='/nutricion'>
-            <button className="w-full h-48 md:h-72 bg-purple rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center">
-              <img className='w-1/2 md:w-3/4' src={sleep} alt="sueño" />
-              <p className="text-white text-xs md:text-base">Sueño</p>
+            <button className="p-3 w-full h-52 md:h-72 bg-purple rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center gap-2">
+              <img className='w-24 md:w-32 mt-2 md:mt-5' src={sleep} alt="sueño" />
+              <p className="text-white text-xl md:text-3xl md:mb-4">Sueño</p>
             </button>
           </Link>
           <Link to='/nutricion'>
-            <button className="w-full h-48 md:h-72 bg-orange rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center">
-              <img className='w-1/2 md:w-3/4' src={saludmental} alt="salud mental" />
-              <p className="text-white text-xs md:text-base">Salud mental</p>
+            <button className="p-3 w-full h-52 md:h-72 bg-orange rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center gap-5">
+              <img className='w-32 md:w-40 mt-2 md:mt-5' src={saludmental} alt="salud mental" />
+              <p className="text-white text-xl md:text-3xl">Salud mental</p>
             </button>
           </Link>
           <Link to='/nutricion'>
-            <button className="p-5 w-full h-52 md:h-72 bg-green rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center gap-4">
-              <img className='w-30 md:w-40' src={deporte} alt="deporte" />
-              <p className="text-white text-xl md:text-3xl">Deporte</p>
+            <button className="p-3 w-full h-52 md:h-72 bg-green rounded-2xl shadow-2xl bg-black md:bg-purple-400 flex flex-col items-center justify-center gap-4">
+              <img className='w-32 md:w-40' src={deporte} alt="deporte" />
+              <p className="text-white text-xl md:text-3xl ">Deporte</p>
             </button>
           </Link>
         </div>
