b532858ffe4c0d671de53678b7a89e96d4880c76
Rocio <rgermillaclopez@gmail.com>
2023-03-16 14:42:48 +0100

    footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const Footer : React.FC = () => {
+  return (
+    <>
+        
+
+    <footer className="bg-orange text-white text-3xl hidden md:flex flex-col fixed bottom-0 justify-center items-center h-32 
+    backdrop-blur-sm w-full box-border">
+      <h1>© Habit+ 2023</h1>
+    </footer>
+
+    </>
+  )
+}
+
+export default Footer
