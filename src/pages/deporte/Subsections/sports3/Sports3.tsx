b8c2db43222fbc80bcfbca70738cb4da38e2b66e
Rocio <rgermillaclopez@gmail.com>
2023-03-16 21:08:40 +0100

    sections added with write content

--- src/pages/deporte/Subsections/sports3/Sports3.tsx
+++ src/pages/deporte/Subsections/sports3/Sports3.tsx
@@ -13,6 +13,21 @@ const Sports3 = () => {
     <div className='flex justify-center mt-10'>
     <h1 className='text-black text-center h-fit w-fit p-2 bg-yellow rounded-lg md:text-3xl'>CONSEJOS PARA REALIZAR ACTIVIDAD FÍSICA</h1>  
     </div>
+    <div className='justify-center p-5'>
+      <p className='text-justify text-xl md:text-6xl'>Empezar a hacer ejercicio requiere que tengamos en consideración una <br />
+        serie de consejos, como:</p>
+      <ul> 
+       <li className='text-justify text-xl md:text-6xl'>Antes de cualquier práctica deportiva, calentar el cuerpo.</li> 
+       <li className='text-justify text-xl md:text-6xl'> Ajustar la intensidad y el tipo de ejercicio a nuestras posibilidades, <br />
+      empezando por ejercicios sencillos y pocas repeticiones e ir aumentando.</li>
+       <li className='text-justify text-xl md:text-6xl'> Mantenerse bien hidratado/a durante la actividad física.</li>
+       <li className='text-justify text-xl md:text-6xl'> Después de hacer ejercicio, ducharse y lavar la ropa.</li>
+      </ul>
+      <h2 className='text-justify text-xl md:text-6xl'>Puedes ver el siguiente video con recomendaciones de la OMS <br />
+      sobre actividades físicas:https://www.youtube.com/watch?v=DcZO3YzbWuA</h2>
+
+    </div>
+  
     <Footer/>
     </>
   )
--- src/pages/deporte/Subsections/sports4/Sports4.tsx
+++ src/pages/deporte/Subsections/sports4/Sports4.tsx
@@ -13,6 +13,21 @@ const Sports4 = () => {
     <div className='flex justify-center mt-10'>
     <h1 className='text-black text-center h-fit w-fit p-2 bg-yellow rounded-lg md:text-3xl'>ACTIVIDAD FÍSICA EN EL ENTORNO</h1>  
     </div>
+
+    <div className='justify-center p-3'>
+      <p className='text-justify md:text-6xl'>
+      Apuntarse a un gimnasio no es la única opción para hacer ejercicio. Hay muchas <br />
+      propuestas que se pueden hacer en casa o en nuestro barrio. <br />
+      Puedes consultar el siguiente documento: <br />
+      http://www.estilosdevidasaludable.mscbs.gob.es/<br />
+      Decargarte aplicaciones gratuitas: Mi Fit, Strava, Pedometer, Period <br />
+      Traker, Health Sync, Step Tracker o Gym Fitness & Workout y para IOS, <br />
+      MGerhonte, Strava, Fitness Coach y Home Workout. <br />
+      O, ver videos o seguir canales de youtube: Gym Virtual, Vitónica, <br />
+      FitnessBlender y Xuan Lan Yoga.
+      </p>
+    </div>
+
     <Footer/>
     </>
   )
