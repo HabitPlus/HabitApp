e6ad9a9523df2f908b7d164cfb91af9bd8a33c34
victornietof5 <vihugo317f5@gmail.com>
2023-03-17 18:41:22 +0100

    modified scrolling and routes

--- src/App.tsx
+++ src/App.tsx
@@ -19,21 +19,23 @@ import Efectos from "./pages/sueño/subsecciones/Efectos";
 import Estrategias from "./pages/sueño/subsecciones/Estrategias";
 import Habitos from "./pages/sueño/subsecciones/Habitos";
 import Insomnio from "./pages/sueño/subsecciones/Insomnio";
-
-
-
+import { BackButton } from "./components/Arrow";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/nutricion" element={<Nutricion />} />
+      <div className="container">
+        
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/nutricion" element={<Nutricion />} />
 
 
-          <Route path="/sueño" element={<Sueño backgroundColor="#CCEBFB"/>}/>
+            <Route path="/sueño" element={<Sueño backgroundColor="#CCEBFB" />} />
             <Route path="/dormir" element={<Dormir />} />
             <Route path="/consultarPro" element={<ConsultarPro />} />
             <Route path="/insomnio" element={<Insomnio />} />
@@ -41,17 +43,39 @@ function App() {
             <Route path="/habitos" element={<Habitos />} />
             <Route path="/estrategias" element={<Estrategias />} />
 
-          <Route path="/salud-mental" element={<SaludMental />} />
-          <Route path="/deporte" element={<Deporte backgroundColor="#F8EFD4" />} />
-          <Route path="/contact" element={<Contact backgroundColor="#FFFCF3" />} />
-          <Route path="/sports1" element={<Sports1 />} />
-          <Route path="/sports2" element={<Sports2 />} />
-          <Route path="/sports3" element={<Sports3 />} />
-          <Route path="/sports4" element={<Sports4 />} />
-        </Route>
-      </Routes>
+            <Route path="/salud-mental" element={<SaludMental />} />
+
+            <Route path="/deporte" element={<Deporte backgroundColor="#F8EFD4" />} />
+            <Route path="/contact" element={<Contact backgroundColor="#FFFCF3" />} />
+            <Route path="/sports1" element={<Sports1 />} />
+            <Route path="/sports2" element={<Sports2 />} />
+            <Route path="/sports3" element={<Sports3 />} />
+            <Route path="/sports4" element={<Sports4 />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
 
 export default App;
+
+// import React from 'react'
+// import "./App.css";
+// import { BrowserRouter } from "react-router-dom";
+// import Routes from './routes/Routes';
+
+
+// const App = () => {
+//   return (
+//     <>
+    
+//     <div className="container">
+//     <Routes />
+//     </div>
+   
+//     </>
+//   )
+// }
+
+// export default App
--- src/pages/sueño/subsecciones/Dormir.tsx
+++ src/pages/sueño/subsecciones/Dormir.tsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { BackButton } from '../../../components/Arrow'
 import Header from '../../../components/Header'
 import Nav from '../../../components/Nav'
+import dormirbien from '../../sueño/dormirbien.jpg'
 
 const Dormir = () => {
   return (
     <>
-    <Header/>
-    <BackButton/>
-    <Nav/>
-    
-    <div className='flex flex-col items-center justify-center text-center mt-10 px-4 md:px-0'>
+      <Header />
+      <BackButton />
+      <Nav />
+      <div className='flex flex-col items-center justify-center text-center mt-10 px-4 md:px-0'>
         <h1 className='text-black font-bold text-xl md:text-6xl p-4 mb-6 py-4 md:p-6 bg-blue rounded-lg '>El Dormir Bien</h1>
         <p className='text-black text-justify text-lg md:text-xl leading-8'>
           Dormir bien es una necesidad del ser humano pues el sueño permite que nuestro cuerpo se recupere del desgaste del día a día. No todo el mundo necesita dormir las mismas horas y con la vida van cambiando nuestras necesidades de horas de sueño. <br /><br />
+        </p>
+        <img src={dormirbien} alt='Dormir bien' className='my-8 w-full md:w-auto max-h-96' />
+        <p className='text-black text-justify text-lg md:text-xl leading-8'>
           Dormir bien no es sólo dedicarle un número de horas, ni demasiadas más, ni menos, es también tener unos hábitos a la hora de ir a dormir.
         </p>
       </div>
-    
-    
-
     </>
   )
 }
