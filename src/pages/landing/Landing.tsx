e4a76118dcc8974beedf2fb69b2046cdece74e5f
victornietof5 <vihugo317f5@gmail.com>
2023-03-20 13:11:09 +0100

    merge feature/sueño

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import chica from '../../assets/girl-home1.svg'
-import logo from '../../assets/logo-caritas.png'
 import Header from '../../components/Header'
 import './Landing.css'
 
@@ -10,9 +9,9 @@ const Landing = () => {
   return (
     <>
     <Header />
-      <div className='flex flex-col items-center justify-center mx-4'>
-        <div className='flex flex-col md:flex-row items-center justify-center gap-2 mt-14'>
-          <img src={chica} alt="chica-corriendo" className='w-full md:w-2/3 md:max-xl:' />
+      <div className='flex flex-col items-center justify-center h-screen -mt-16'>
+        <div className='flex flex-col md:flex-row items-center justify-center gap-2'>
+          <img src={chica} alt="chica-corriendo" className='chica-img' />
           <Link to="/home">
             <button className="mx-auto mt-8 font-serif text-white font-weight: 600 bg-orange hover:bg-red rounded-full p-3 w-64 h-10 tracking-[1rem] mb-5 md:ml-10 transition-colors duration-300">
               Habit+
--- src/pages/sueño/Sueño.tsx
+++ src/pages/sueño/Sueño.tsx
@@ -4,12 +4,32 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Link } from 'react-router-dom';
 import Carousel from '../sueño/Carrusel';
+import Header from '../../components/Header';
+import { BackButton } from '../../components/Arrow';
+import Nav from '../../components/Nav';
+import Carrusel from '../sueño/Carrusel';
 
-const Sueño = () => {
+
+interface SueñoProps {
+  backgroundColor: string;
+}
+const Sueño: React.FC<SueñoProps> = ({ backgroundColor }) => {
   return (
     <>
-      <Carousel />
-      <h1>Habit+</h1>
+      <Header />
+      <BackButton />
+      <Nav />
+      <>
+        <h1 className='text-black text-center text-3xl font-semibold mt-14'>Sueño</h1>
+        <Carrusel />
+        <style type="text/css">
+          {`
+          body {
+            background-color: ${backgroundColor};
+          }
+        `}
+        </style>
+      </>
     </>
   );
 };
