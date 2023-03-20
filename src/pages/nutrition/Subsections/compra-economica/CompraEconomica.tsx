25c8ef443857bd2daa51fa5d1a1819a2abec5667
albapro <albamaria.idi@gmail.com>
2023-03-20 11:48:15 +0100

    video youtube

--- src/pages/nutrition/Subsections/compra-economica/CompraEconomica.tsx
+++ src/pages/nutrition/Subsections/compra-economica/CompraEconomica.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import YouTube from 'react-youtube'
 import { BackButton } from '../../../../components/Arrow'
 import Footer from '../../../../components/Footer'
 import Header from '../../../../components/Header'
 import Nav from '../../../../components/Nav'
 
-type Props = {}
-
-const CompraEconomica = (props: Props) => {
+const CompraEconomica = () => {
   return (
     <>
       <Header />
@@ -20,18 +19,21 @@ const CompraEconomica = (props: Props) => {
           <h1>¿Cómo ahorrar en la compra de alimentos? </h1>
         </article>
         <article className='grid grid-rows-1 p-10 md:text-2xl'>
-          <p> Elegir el qué y dónde se compra puede ser una clave para el ahorro. Para que nuestra compra pueda ser más económica, los expertos recomiendan:<br></br>
-            <p className='mt-3'>•	Apostar por marcas blancas en aquellos productos en los que no se note la diferencia. <br></br></p>
-            <p className='mt-3'> •	Elegir supermercados que son más económicos. La OCU publica un listado de estos. <br></br></p>
-            <p className='mt-3'>•	Hacer una lista detallada con lo que se necesita antes de ir a comprar y seguirla sin dejarse llevar por tentaciones u ofertas. <br></br></p>
-            <p className='mt-3'>•	Evitar ir a comprar con hambre. <br></br></p>
-            <p className='mt-3'>•	Comparar los precios de los productos similares. <br></br></p>
-            <p className='mt-3'>•	Aprovechar lo que ya se tiene en casa. <br></br></p>
-            <p className='mt-3'>•	Aprovechar productos de temporada. <br></br></p>
-            <p className='mt-3'>•	Aprovechar tarjetas de fidelización y descuentos. <br></br></p>
-            <p className='mt-3'>•	Revisar los tíquets de la compra después de pagar. <br></br></p>
-            <p className='mt-3'>•	Realizar una compra semanal. <br></br></p>
-          </p>
+          <p> Elegir el qué y dónde se compra puede ser una clave para el ahorro. Para que nuestra compra pueda ser más económica, los expertos recomiendan:<br></br></p>
+          <p className='mt-3'>•	Apostar por marcas blancas en aquellos productos en los que no se note la diferencia. <br></br></p>
+          <p className='mt-3'>•	Elegir supermercados que son más económicos. La OCU publica un listado de estos. <br></br></p>
+          <p className='mt-3'>•	Hacer una lista detallada con lo que se necesita antes de ir a comprar y seguirla sin dejarse llevar por tentaciones u ofertas. <br></br></p>
+          <p className='mt-3'>•	Evitar ir a comprar con hambre. <br></br></p>
+          <p className='mt-3'>•	Comparar los precios de los productos similares. <br></br></p>
+          <p className='mt-3'>•	Aprovechar lo que ya se tiene en casa. <br></br></p>
+          <p className='mt-3'>•	Aprovechar productos de temporada. <br></br></p>
+          <p className='mt-3'>•	Aprovechar tarjetas de fidelización y descuentos. <br></br></p>
+          <p className='mt-3'>•	Revisar los tíquets de la compra después de pagar. <br></br></p>
+          <p className='mt-3'>•	Realizar una compra semanal. <br></br></p>
+          <p className='mt-3'>Mira el siguiente video de Paula Simple que explica estas y otras claves:<br></br></p>
+        </article>
+        <article className='grid grid-cols-1 justify-center'>
+           <YouTube videoId='https://www.youtube.com/watch?v=MlcH1TocMSA'/>
         </article>
       </>
     </>
