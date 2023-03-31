96e965e6afb96d7a2b9ace47fb465b88c0783558
albapro <albamaria.idi@gmail.com>
2023-03-31 13:12:50 +0200

    added contact to footer

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BackButton } from '../../components/arrow/Arrow'
 import Header from '../../components/header/Header'
 import Nav from '../../components/nav/Nav'
+import Footer from '../../components/footer/Footer'
 
 interface contactProps {
   backgroundColor: String;
@@ -13,10 +14,12 @@ const contact: React.FC<contactProps> = (backgroundColor) => {
       <Header />
       <BackButton />
       <Nav bgColor={"#cc1126"} />
-      <div className='mt-32 flex flex-col text-red text-center px-8'>
-        <h1 className='text-3xl my-6 font-serif text-center'>Contacta con Caritas</h1>
+      <Footer bgColor={"#cc1126"} />
+      <div className='mt-32 md:mt-10 flex flex-col text-red text-center px-8'>
+        <h1 className='text-3xl my-14 font-serif text-center'>Contacta con Càritas</h1>
+        <p>Dirección: C/Armenteres, 35, 08980, Sant Feliu de Llobregat</p>
         <a href="mailto:habitsplus@caritassantfeliu.cat?body=My custom mail body"><h3 className='my-6 text-lg'>E-mail: habitsplus@caritassantfeliu.cat</h3></a>
-        <a href="tel:+34 936 255 770"><h3 className='text-lg'>O llamando al +34 936 255 770</h3></a>
+        <a href="tel:+34 936 255 770"><h3 className='text-lg'>Ó llamando al +34 936 255 770</h3></a>
         <style type="text/css">
           {`
           body {
