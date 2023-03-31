be49527d928b95e3f8debc14ec1b1c17bf2b6924
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-31 09:22:09 +0200

    added website link in CONTACT PAGE

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -15,8 +15,13 @@ const contact: React.FC<contactProps> = (backgroundColor) => {
       <Nav bgColor={"#cc1126"} />
       <div className='mt-32 flex flex-col text-red text-center px-8'>
         <h1 className='text-3xl my-6 font-serif text-center'>Contacta con Caritas</h1>
-        <a href="mailto:habitsplus@caritassantfeliu.cat?body=My custom mail body"><h3 className='my-6 text-lg'>E-mail: habitsplus@caritassantfeliu.cat</h3></a>
-        <a href="tel:+34 936 255 770"><h3 className='text-lg'>O llamando al +34 936 255 770</h3></a>
+       
+        <a href="mailto:habitsplus@caritassantfeliu.cat?body=My custom mail body"><h3 className='my-6 text-lg'><h3 className='underline'> E-mail:</h3> habitsplus@caritassantfeliu.cat</h3></a>
+        
+        <a href="tel:+34 936 255 770"><h3 className='text-lg'>O llamando al +34 936 255 770</h3></a><br />
+        
+        <a href="https://www.caritassantfeliu.cat/"><h3 className='text-lg underline'>https://www.caritassantfeliu.cat/</h3></a>
+        
         <style type="text/css">
           {`
           body {
